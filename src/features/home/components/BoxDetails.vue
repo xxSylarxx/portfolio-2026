802deb1ad281d90b13ad788d422ad5842712ba8f
Axel Molina <axelmol2018@gmail.com>
2026-05-20 07:14:45 -0500

    update

--- src/features/home/components/BoxDetails.vue
+++ src/features/home/components/BoxDetails.vue
@@ -104,7 +104,7 @@ const handleTimelineCreated = (timeline: gsap.core.Timeline, delay: number) => {
       <div class="box-details-content">
         <div class="box-details-title">
           <AppearingText
-            text="David"
+            text="Axel"
             :steps="1"
             :duration="0.35"
             @timeline:created="(tl: gsap.core.Timeline) => handleTimelineCreated(tl, 0)"
